efeba4fd393a34d1f05702297499958f464d5b68
Luisa Carvalho <carvalholuisa2006@gmail.com>
2026-06-06 23:11:46 -0300

    feat: adiciona tela de login e estilização inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Login from './components/Login/Login';
 
 function App() {
 
   return (
     <div className="App">
-      <p>Página de Login</p>
+      <Login />
     </div>
   );
 }
